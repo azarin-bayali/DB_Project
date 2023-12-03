189755e8127ef35fac143e2d580c76b8ab7fa9d6
azarin-bayali <81379164+azarin-bayali@users.noreply.github.com>
2023-12-03 15:52:48 +0400

    Connection Establishing

--- src/com/company/Functions.java
+++ src/com/company/Functions.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class Functions {
+    public Connection connection_toDB(String dbName, String username, String password){
+        Connection con = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, username, password);
+            if(con != null) System.out.println("Connection made!");
+            else System.out.println("Connection failed!");
+        }catch (Exception e){
+            System.out.println("Exception Found!");
+        }
+         return con;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Functions db = new Functions();
+        db.connection_toDB("BookApp", "Azarin", "HelloThere!")
     }
 }
