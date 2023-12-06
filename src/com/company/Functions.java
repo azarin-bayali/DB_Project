775a132d31d046fa136be84c22e3049759b3c3c5
azarin-bayali <81379164+azarin-bayali@users.noreply.github.com>
2023-12-06 16:58:02 +0400

    Inserting Values to tables

--- src/com/company/Functions.java
+++ src/com/company/Functions.java
@@ -1,23 +1,23 @@
 package com.company;
 
 import java.sql.*;
-import java.util.Arrays;
 
 //Connecting to Database
 public class Functions {
-    public Connection connection_toDB(String dbName, String username, String password){
+    public Connection connection_toDB(String dbName, String username, String password) {
         Connection con = null;
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, username, password);
-            if(con != null) System.out.println("Connection made!");
+            if (con != null) System.out.println("Connection made!");
             else System.out.println("Connection failed!");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
 
-         return con;
+        return con;
     }
+
     //CRUD Operations
     // 1. Inserting new records
     public void insertBook(String title, String isbn, int authorID, int stockQuantity, double price, String publicationDate) {
@@ -39,6 +39,53 @@ public class Functions {
         }
     }
 
+    // 2. Retrieving all book information, including authors and associated orders
+    public void getAllBookInfo() {
+            try {
+                Connection con = connection_toDB("dbProject", "postgres", "12073");
+                String sql = "SELECT * FROM Books " +
+                        "JOIN Authors ON Books.AuthorID = Authors.AuthorID " +
+                        "LEFT JOIN Orders_Books ON BookGeneres.BookID = Orders_Books.BookID " +
+                        "LEFT JOIN Orders ON Orders_Books.OrderID = Orders.OrderID";
+                try (Statement stmt = con.createStatement();
+                     ResultSet rs = stmt.executeQuery(sql)) {
+                    while (rs.next()) {
+                        int bookID = rs.getInt("BookGeneres.BookID"); // Corrected
+                        String title = rs.getString("Books.Title");
+                        String isbn = rs.getString("Books.ISBN");
+                        int authorID = rs.getInt("Books.AuthorID"); // Corrected
+                        String authorName = rs.getString("Authors.AuthorName");
+                        int stockQuantity = rs.getInt("Books.StockQuantity");
+                        double price = rs.getDouble("Books.Price");
+                        java.sql.Date publicationDate = rs.getDate("Books.PublicationDate");
+                        int orderID = rs.getInt("Orders.OrderID");
+                        java.sql.Date orderDate = rs.getDate("Orders.OrderDate");
+                        double totalAmount = rs.getDouble("Orders.TotalAmount");
+                        String status = rs.getString("Orders.Status");
+
+                        System.out.println("BookID: " + bookID);
+                        System.out.println("Title: " + title);
+                        System.out.println("ISBN: " + isbn);
+                        System.out.println("AuthorID: " + authorID);
+                        System.out.println("AuthorName: " + authorName);
+                        System.out.println("StockQuantity: " + stockQuantity);
+                        System.out.println("Price: " + price);
+                        System.out.println("PublicationDate: " + publicationDate);
+                        System.out.println("OrderID: " + orderID);
+                        System.out.println("OrderDate: " + orderDate);
+                        System.out.println("TotalAmount: " + totalAmount);
+                        System.out.println("Status: " + status);
+                        System.out.println();
+                    }
+                }
+            } catch (SQLException e) {
+                System.out.println("Error retrieving book information: " + e.getMessage());
+            }
+        }
+    }
+
+
+
 
 
 
@@ -50,40 +97,40 @@ public class Functions {
 
 
     //Selecting books
-    public void selectBook(int bookID) {
-        try {
-            Connection con = connection_toDB("dbProject", "postgres", "12073");
-            String sql = "SELECT * FROM Books WHERE BookID = ?";
-            try (PreparedStatement pstmt = con.prepareStatement(sql)) {
-                pstmt.setInt(1, bookID);
-                try (ResultSet rs = pstmt.executeQuery()) {
-                    if (rs.next()) {
-                        // Process and print book details
-                        int retrievedBookID = rs.getInt("BookID");
-                        String title = rs.getString("Title");
-                        String isbn = rs.getString("ISBN");
-                        int authorID = rs.getInt("AuthorID");
-                        int stockQuantity = rs.getInt("StockQuantity");
-                        double price = rs.getDouble("Price");
-                        String publicationDate = rs.getString("PublicationDate");
-
-                        System.out.println("BookID: " + retrievedBookID);
-                        System.out.println("Title: " + title);
-                        System.out.println("ISBN: " + isbn);
-                        System.out.println("AuthorID: " + authorID);
-                        System.out.println("StockQuantity: " + stockQuantity);
-                        System.out.println("Price: " + price);
-                        System.out.println("PublicationDate: " + publicationDate);
-                    } else {
-                        System.out.println("Book with ID " + bookID + " not found.");
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println("Error selecting book: " + e.getMessage());
-        }
-    }
-}
+//    public void selectBook(int authorID) {
+//        try {
+//            Connection con = connection_toDB("dbProject", "postgres", "12073");
+//            String sql = "SELECT * FROM Books WHERE AuthorID = ?";
+//            try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+//                pstmt.setInt(1, authorID);
+//                try (ResultSet rs = pstmt.executeQuery()) {
+//                    if (rs.next()) {
+//                        // Process and print book details
+//                        int retrievedBookID = rs.getInt("BookID");
+//                        String title = rs.getString("Title");
+//                        String isbn = rs.getString("ISBN");
+//                        authorID = rs.getInt("AuthorID");
+//                        int stockQuantity = rs.getInt("StockQuantity");
+//                        double price = rs.getDouble("Price");
+//                        String publicationDate = rs.getString("PublicationDate");
+//
+//                        System.out.println("BookID: " + retrievedBookID);
+//                        System.out.println("Title: " + title);
+//                        System.out.println("ISBN: " + isbn);
+//                        System.out.println("AuthorID: " + authorID);
+//                        System.out.println("StockQuantity: " + stockQuantity);
+//                        System.out.println("Price: " + price);
+//                        System.out.println("PublicationDate: " + publicationDate);
+//                    } else {
+//                        System.out.println("Book with ID " + authorID + " not found.");
+//                    }
+//                }
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("Error selecting book: " + e.getMessage());
+//        }
+//    }
+//}
 
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,15 +11,23 @@ public class Main {
         db.connection_toDB(name, username, password);
 
         // Call CRUD operations
+
         //Call insertion
-        db.insertBook("The Great Gatsby", "9780142437266", 1, 100, 25.99, "2022-01-01");
+        //db.insertBook("The Great Gatsby", "9780142437266", 1, 100, 25.99, "2022-01-01");
+
+        // Retrieving all book information
+        db.getAllBookInfo();
+
 
         // Call Transaction
 
         // Call Metadata Access
 
 
-        
+        //db.selectBook(1);
+
+
+
     }
 }
 
